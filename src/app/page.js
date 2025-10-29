81920e6c2662b09f04e216c6b72a72a2b3c64a07
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-10-29 11:09:13 +0530

    Add Hero component with interactive LaserFlow effect to the homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,15 @@
+import Hero from "@/components/Hero";
+import LaserFlow from "@/components/ui/LaserFlow";
 import Image from "next/image";
+
 
 export default function Home() {
   return (
-    <div>
-      hello
+    <div className="w-full h-screen">
+      {/* <div className="reative w-full h-screen overflow-hidden">
+  <LaserFlow />
+</div> */}
+<Hero/>
     </div>
   );
 }
