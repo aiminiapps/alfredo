e83434181d62723c02e173ead8abcc7c7b8a3c55
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-10-30 10:17:36 +0530

    Remove unused LaserFlow component from Home page for cleaner code

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,9 @@
 import Hero from "@/components/Hero";
-import LaserFlow from "@/components/ui/LaserFlow";
-import Image from "next/image";
-
 
 export default function Home() {
   return (
     <div className="w-full h-screen">
-      {/* <div className="reative w-full h-screen overflow-hidden">
-  <LaserFlow />
-</div> */}
-<Hero/>
+      <Hero/>
     </div>
   );
 }
