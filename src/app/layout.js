d8faa5c7b8c6e2dc8810e5fb893affaec1cc8a23
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-11-06 13:45:06 +0530

    GA added in the layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -249,6 +250,7 @@ export default function RootLayout({ children }) {
       >
         {children}
       </body>
+      <GoogleAnalytics gaId="G-XYZ" />
     </html>
   );
 }
